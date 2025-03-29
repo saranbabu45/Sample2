355e09c23e4b6332b813fb02d4cdbb6c7bd858c6
saranbabu09 <babusaran785@gmail.com>
2025-03-30 01:13:16 +0530

    COMMITTING FROM FEATURE

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-//sss
+//THIS COMMIT IS FROM FEATURE BRANCH - LETS ROCK
     }
 }
